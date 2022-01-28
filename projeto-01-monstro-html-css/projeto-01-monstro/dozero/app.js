bc0aa610a1bbfb4b210161069f98ad8014e9732c
Gabriel Couto <gabriel.couto@qwst.co>
2022-01-28 16:05:50 -0300

    fix for in atributos,modelo vue projeto monstro

--- projeto-01-monstro-html-css/projeto-01-monstro/dozero/app.js
+++ projeto-01-monstro-html-css/projeto-01-monstro/dozero/app.js
@@ -1,4 +1,26 @@
+//@ts-check
 new Vue({
     el: '#app',
-   
+    data:{
+        barra: '',
+        vidaJogador:100,
+        vidaMonstro:100
+    },
+   methods:{
+
+   },
+   computed:{
+    vidaJogadorProgresso(){
+        
+        let n = 0
+        
+           return{
+               backgroundColor:'red',
+               height:'112px',
+               width:'10%'
+           }
+       }
+
+   }
+
 })
