9067f78380d94be936787a96bc8fad1b52598434
Gabriel Couto <gabriel.couto@qwst.co>
2022-01-28 21:10:32 -0300

    fix for in atributos,modelo vue projeto monstro

--- projeto-01-monstro-html-css/projeto-01-monstro/dozero/app.js
+++ projeto-01-monstro-html-css/projeto-01-monstro/dozero/app.js
@@ -1,26 +1,51 @@
 //@ts-check
 new Vue({
     el: '#app',
-    data:{
+    data: {
         barra: '',
-        vidaJogador:100,
-        vidaMonstro:100
+        vidaJogador: 100,
+        vidaMonstro: 100
     },
-   methods:{
+    methods: {
+        calculaVida(valor, cor) {
+            let larguraVida = this.vidaJogador / 4
+            let larguraVw = larguraVida + 'vw'
 
-   },
-   computed:{
-    vidaJogadorProgresso(){
-        
-        let n = 0
-        
-           return{
-               backgroundColor:'red',
-               height:'112px',
-               width:'10%'
-           }
-       }
+            return {
+                backgroundColor: cor,
+                width: larguraVida + 'vw'
+            }
+        },
+        alteraVidaJogadorMonstro(vidaJogador,
+            vidaMonstro) {
+            this.vidaJogador += vidaJogador
+            this.vidaMonstro += vidaMonstro
 
-   }
+        },
+        ataque() {
+            
+            this.alteraVidaJogadorMonstro(-7, -5)
+        },
+        ataqueEspecial(){
+            this.alteraVidaJogadorMonstro(-5,-7)
+        },
+        cura(){
+            this.alteraVidaJogadorMonstro(5,-6)
+        },
+        desistir(){
+
+        }
+    },
+    computed: {
+        vidaJogadorProgresso() {
+            return this.calculaVida(this.vidaJogador, 'red')
+        },
+        
+
+        vidaMonstroProgresso() {
+            return this.calculaVida(this.vidaMonstro, 'green')
+        }
+
+    },
 
 })
