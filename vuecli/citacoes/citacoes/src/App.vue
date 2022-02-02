d80164960fef900b537a7a2b6dfa8680e48cc71e
Gabriel Couto <gabriel.couto@qwst.co>
2022-02-02 13:05:48 -0300

    vslots

---
+++ vuecli/citacoes/citacoes/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Citacoes @onIndiceMudou="mudarCitacaoSelecionada" />
+    <Citacao :citacao="citacaoSelecionada" />
+    <Sobre />
+  </div>
+</template>
+
+<script>
+import Citacao from "./components/Citacao.vue";
+import Citacoes from "./components/Citacoes";
+import Sobre from "./components/Sobre";
+
+export default {
+  components: { Citacoes, Sobre, Citacao },
+  data() {
+    return {
+      citacaoSelecionada: null,
+    };
+  },
+  methods: {
+    mudarCitacaoSelecionada(citacao){
+		this.citacaoSelecionada=citacao
+	}
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 1.6rem;
+}
+
+button {
+  outline: none;
+  padding: 5px 15px 10px;
+  margin: 10px 5px;
+  border-radius: 3px;
+  font-size: 2rem;
+  font-weight: 300;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ vuecli/citacoes/citacoes/src/components/Citacao.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="citacao" v-if="citacao">
+        <p>{{citacao.texto}}</p>
+        <small>{{citacao.fonte}}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        citacao:Object
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
