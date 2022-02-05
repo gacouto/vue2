1a918ed5a5eae5475500b522b766951902d39ac1
Gabriel Couto <gabriel.couto@qwst.co>
2022-02-05 12:46:31 -0300

    directive follow element

---
+++ pos-projeto2/diretivas-desafio/diretivas-desafio/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <h1>Diretivas (Desafio)</h1>
+    <hr />
+    <!-- Exercício -->
+    <!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+    <button v-event-changer:followyou>change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    "event-changer": {
+      bind(el, binding, vnode) {
+        if (binding.arg === "followyou") {
+          //debugger
+		  let moveElement = function (windowevent){			  
+				  let xPercent = (windowevent.x / window.innerWidth).toFixed(2);
+				  let yPercent = (windowevent.y / window.innerHeight).toFixed(2);
+				  
+				  console.log(`${xPercent} x ${yPercent}`); 
+				  console.log(`${el.style.top}`);
+				  
+				  el.style.position = "fixed";
+				  el.style.top = `${(yPercent*100)-5}%`;
+				  el.style.left = `${(xPercent*100)}%`;
+			  }
+		  
+          el.addEventListener("click", function (event) {
+			  
+			  if(el.style.position === "fixed"){
+				  el.style.position === "static"
+				  window.removeEventListener('mousemove',moveElement)
+			  }else{				  
+				  window.addEventListener('mousemove',moveElement)
+			  }
+          });
+        }
+      },
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+
+button {
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
+}
+</style>
