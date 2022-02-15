6f8629d5e72d3e265f30cd1aa000c84bb6d9445d
Gabriel Couto <gabriel.couto@qwst.co>
2022-02-15 15:41:31 -0300

    scoped slots test

---
+++ httpaxios/stck2/stock-trader/stock-trader/src/components/BarTeste.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-alert
+      :value="true"
+      color="info"
+      icon="info"
+      outline    >
+      Você pode salvar 
+      <slot :propReversa="'callMeBack'"></slot>
+       e carregar dados
+    </v-alert>
+</template>
+<script>
+export default {
+  methods:{
+    callMeBack(){
+      console.log('MANDA BRAZA')
+    }
+  }
+}
+</script>
--- httpaxios/stck2/stock-trader/stock-trader/src/components/Home.vue
+++ httpaxios/stck2/stock-trader/stock-trader/src/components/Home.vue
@@ -1,30 +1,28 @@
 <template>
-    <div>
-        <h1>Negocie e consult suas açoes</h1>
-         <v-alert
-      :value="true"
-      color="info"
-      icon="info"
-      outline    >
+  <div>
+    <h1>Negocie e consult suas açoes</h1>
+    <v-alert :value="true" color="info" icon="info" outline>
       Você pode salvar e carregar dados
     </v-alert>
-    
-     <v-alert
-      :value="true"
-      color="success"
-      icon="check_circle"
-      outline    >
+    <BarTeste>
+      <template slot-scope="props">
+        <strong>beibe {{ props.propReversa }} beibe</strong>
+      </template>
+    </BarTeste>
+    <v-alert :value="true" color="success" icon="check_circle" outline>
       Clique em finalizar dia para flutuar os preços
     </v-alert>
-    <h2>Seu saldo é   {{getTodaysBalanceFormatted}}</h2>
-    
-    </div>
+    <h2>Seu saldo é {{ getTodaysBalanceFormatted }}</h2>
+  </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
+import BarTeste from "./BarTeste.vue";
 export default {
-    computed:{
-        ...mapGetters(["getTodaysBalanceFormatted"])
-    }
-}
+  components: { BarTeste },
+
+  computed: {
+    ...mapGetters(["getTodaysBalanceFormatted"]),
+  },
+};
 </script>
